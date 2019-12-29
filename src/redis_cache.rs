2ddd381fa42ff26dba220f6fc5149cb4a7b12497
Insanitybit <insanitybit@gmail.com>
2019-12-29 13:10:33 -0800

    Move to darkredis

--- src/redis_cache.rs
+++ src/redis_cache.rs
@@ -1,6 +1,7 @@
 use log::warn;
 use futures::compat::Future01CompatExt;
-use simple_redis::{client::Client as RedisClient, RedisError};
+use darkredis::ConnectionPool;
+use darkredis::Error as RedisError;
 
 use async_trait::async_trait;
 
@@ -9,12 +10,20 @@ use crate::cache::{Cache, Cacheable, CacheResponse};
 #[derive(Clone)]
 pub struct RedisCache {
     address: String,
+    connection_pool: ConnectionPool,
 }
 
 impl RedisCache {
-    pub fn new(address: String) -> Result<Self, RedisError> {
+    pub async fn new(address: String) -> Result<Self, RedisError> {
+        let connection_pool = ConnectionPool::create(
+            "127.0.0.1:6379".into(),
+            None,
+            num_cpus::get()
+        ).await?;
+
         Ok(
             Self {
+                connection_pool,
                 address,
             }
         )
@@ -22,20 +31,18 @@ impl RedisCache {
 }
 
 #[async_trait]
-impl Cache<String> for RedisCache {
-    async fn get<CA>(&mut self, cacheable: CA) -> Result<CacheResponse, String>
+impl Cache<RedisError> for RedisCache {
+    async fn get<CA>(&mut self, cacheable: CA) -> Result<CacheResponse, RedisError>
         where
             CA: Cacheable + Send + Sync + 'static
     {
         let identity = cacheable.identity();
         let identity = hex::encode(identity);
 //
-        let res = simple_redis::create(&self.address)
-            .map_err(|e| format!("{:?}", e))
-            .and_then(|mut client| {
-                client.exists(&identity)
-                    .map_err(|e| format!("{:?}", e))
-            });
+        let mut client = self.connection_pool.get().await;
+
+        let res = client.exists(&identity).await;
+
         match res {
             Ok(true) => Ok(CacheResponse::Hit),
             Ok(false) => Ok(CacheResponse::Miss),
@@ -46,16 +53,14 @@ impl Cache<String> for RedisCache {
         }
     }
 
-    async fn store(&mut self, identity: Vec<u8>) -> Result<(), String>
+    async fn store(&mut self, identity: Vec<u8>) -> Result<(), RedisError>
     {
         let identity = hex::encode(identity);
 
-        simple_redis::create(&self.address)
-            .map_err(|e| format!("{:?}", e))
-            .and_then(|mut client| {
-                client.set(&identity, true)
-                    .map(|_| ())
-                    .map_err(|e| format!("{:?}", e))
-            })
+        let mut client = self.connection_pool.get().await;
+
+        client.set(&identity, b"1").await?;
+
+        Ok(())
     }
 }
